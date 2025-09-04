51b561ff103d6b20b84d21d8523839f4df955f49
Scarlet Fate <michaelemediong13@gmail.com>
2025-09-04 16:29:39 +0100

    commit all

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -28,7 +28,7 @@ const ProjectDetail = (
             bg-slate-900 hover:bg-gray-500'>
                 <IconX className="text-black dark:text-white" onClick={onClick} />
             </button>
-            <img src="/src/assets/Boy-fitness.png" alt={title} className='w-full rounded-t-2xl' />
+            <img src="/public/Boy-fitness.png" alt={title} className='w-full rounded-t-2xl' />
             <Box className='p-5'>
             <Text className='!mb-2 !text-2xl !font-bold !text-white'>{title}</Text>
             <Text className='!mb-3 !font-normal !text-neutral-400'>{description}</Text>
@@ -40,7 +40,6 @@ const ProjectDetail = (
                         className='rounded-lg size-10 animate-bounce'/>
                     )
                     )}
-                </Box>
                 <a href='https://boy-fitness-scarletfate42s-projects.vercel.app/' className='inline-flex items-center justify-center gap-1
                 font-medium cursor-pointer animate-bounce'
                 > View Projects
@@ -49,6 +48,22 @@ const ProjectDetail = (
                 <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6"/><path d="m21 3-9 9"/><path d="M15 3h6v6"/>
                 </svg>
                 </a>
+                </Box>
+                <Box className='flex gap-3'>
+                    {tags.map((tag) =>(
+                        <img key={tag.id} src={tag.path} alt={tag.name} 
+                        className='rounded-lg size-10 animate-bounce'/>
+                    )
+                    )}
+                <a href='https://awards-gamma.vercel.app/' className='inline-flex items-center justify-center gap-1
+                font-medium cursor-pointer animate-bounce'
+                > View Projects
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round" 
+                class="lucide lucide-square-arrow-out-up-right-icon lucide-square-arrow-out-up-right" hhref={href}>
+                <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6"/><path d="m21 3-9 9"/><path d="M15 3h6v6"/>
+                </svg>
+                </a>
+                </Box>
             </Box>
             </Box>
         </motion.div>
